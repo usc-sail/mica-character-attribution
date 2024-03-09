c59342f064d79161467b487aa7b95c6aaf6241ce
sabyasachee <sbaruah@usc.edu>
2024-03-08 20:11:39 -0800

    change env name and install transformers

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-conda create -n represent python=3.11
+conda create -n attr python=3.11
 
 conda install -c conda-forge numpy pandas tqdm selenium absl-py requests beautifulsoup4 unidecode scikit-learn scipy ipykernel matplotlib spacy cupy thefuzz
 
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install conda-forge::transformers
