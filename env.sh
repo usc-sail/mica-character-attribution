cfb33f52a152905d14944c71e1b1d779a7ec1ee5
sabyasachee <sbaruah@usc.edu>
2024-06-02 01:29:39 -0700

    add packages for openai prompting

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 conda create -n attr python=3.11
 
-conda install -c conda-forge numpy pandas tqdm selenium absl-py requests beautifulsoup4 unidecode scikit-learn scipy ipykernel matplotlib spacy cupy thefuzz nameparser jupyterlab ipython cinemagoer
+conda install -c conda-forge numpy pandas tqdm selenium absl-py requests beautifulsoup4 unidecode scikit-learn scipy ipykernel matplotlib spacy cupy thefuzz nameparser jupyterlab ipython cinemagoer openai tiktoken tenacity
 
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install conda-forge::transformers
