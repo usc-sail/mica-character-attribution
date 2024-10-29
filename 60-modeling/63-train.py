01d36044d541b4c2c14c2526895a01977dafd9d5
sabyasachee <sbaruah@usc.edu>
2024-10-28 20:11:34 -0700

    simplify the trainer

--- 60-modeling/63-train.py
+++ 60-modeling/63-train.py
@@ -1,10 +1,10 @@
 """Train the character representation models. This file is the main function that collects the hyperparameters
 and input/output files, trains the model, and evaluates the trained model
 """
-from trainers import trainer
-
 from absl import app
 from absl import flags
+
+from trainers import trainer
 
 FLAGS = flags.FLAGS
 
@@ -15,38 +15,28 @@ flags.DEFINE_enum("inputtype", default="story", enum_values=["story", "character
 flags.DEFINE_string("tokenizermodel", default=None,
                     help=("tokenizer used to preprocess the data; should be a huggingface model; if not provided, it "
                           "defaults to the pretrained_model_name"))
-flags.DEFINE_string("datasetfile", default="60-modeling/dataset-with-only-character-tropes.csv",
-                    help=("csv file containing the character, movie, and trope data; columns include imdb-id, "
-                          "character, trope, component, partition, label"))
-flags.DEFINE_string("tropesfile", default="60-modeling/character-tropes.csv",
-                    help="csv file containing trope name and definition")
 
 # model params
 flags.DEFINE_string("pretrainedmodel", default="roberta-base",
                     help="base encoder to use; should be a huggingface model")
 flags.DEFINE_bool("labeldependent", default=False, help="set to use label-dependent model")
+
+# training params
 flags.DEFINE_integer("nepochs", default=10, help="number of epochs")
 flags.DEFINE_float("encoderlr", default=2e-5, help="learning rate of the encoder")
 flags.DEFINE_float("lr", default=1e-3, help="learning rate of modules apart from the encoder")
 flags.DEFINE_bool("freezeencoder", default=False, help="set to freeze encoder")
-flags.DEFINE_integer("ncharactersbatch", default=5, help=("number of characters per batch when training with "
-                                                          "data type = character"))
+
+# batching params
+flags.DEFINE_integer("ncharactersbatch", default=5,
+                     help="number of characters per batch when training with data type = 'character'")
+flags.DEFINE_integer("ntropesbatch", default=50,
+                     help="number of tropes per batch when training with data type = 'story'")
 
 def start_training(_):
-    tokenizer_model = FLAGS.tokenizermodel if FLAGS.tokenizermodel else FLAGS.pretrainedmodel
-    Trainer = trainer.Trainer(FLAGS.datadir,
-                              FLAGS.datasetfile,
-                              FLAGS.tropesfile,
-                              FLAGS.inputtype,
-                              FLAGS.pretrainedmodel,
-                              tokenizer_model,
-                              FLAGS.labeldependent,
-                              FLAGS.nepochs,
-                              FLAGS.encoderlr,
-                              FLAGS.lr,
-                              FLAGS.freezeencoder,
-                              FLAGS.ncharactersbatch
-                              )
+    print("\n")
+    FLAGS.tokenizermodel = FLAGS.tokenizermodel if FLAGS.tokenizermodel else FLAGS.pretrainedmodel
+    Trainer = trainer.Trainer(FLAGS)
     Trainer()
 
 if __name__ == '__main__':
--- 60-modeling/trainers/trainer.py
+++ 60-modeling/trainers/trainer.py
@@ -7,107 +7,68 @@ from trainers import story_label_dependent_trainer, character_label_dependent_tr
 from trainers import story_label_independent_trainer, character_label_independent_trainer
 
 import os
-import tqdm
-import torch
-import random
-import itertools
-import numpy as np
 import pandas as pd
-from typing import Any
 from torch.optim import AdamW
-from bs4 import BeautifulSoup as bs
 from transformers import AutoTokenizer
 
 class Trainer:
 
-    def __init__(self,
-                 data_dir,
-                 dataset_file,
-                 tropes_file,
-                 data_type,
-                 pretrained_model_name,
-                 tokenizer_model_name,
-                 label_dependent,
-                 n_epochs,
-                 encoderlr,
-                 lr,
-                 freeze_encoder,
-                 ncharacters_batch
-                 ) -> None:
-        self.data_dir = data_dir
-        self.dataset_file = dataset_file
-        self.tropes_file = tropes_file
-        self.data_type = data_type
-        self.pretrained_model_name = pretrained_model_name
-        self.tokenizer_model_name = tokenizer_model_name
-        self.label_dependent = label_dependent
-        self.n_epochs = n_epochs
-        self.encoderlr = encoderlr
-        self.lr = lr
-        self.freeze_encoder = freeze_encoder
-        self.ncharacters_batch = ncharacters_batch
+    def __init__(self, args):
+        self.args = args
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        dataset_file = os.path.join(self.data_dir, self.dataset_file)
-        tropes_file = os.path.join(self.data_dir, self.tropes_file)
-        dataset_df = pd.read_csv(dataset_file, index_col=None, dtype={"imdb-id": str, "content-text": str})
+    def __call__(self):
+        dataset_file = os.path.join(self.args.datadir, "CHATTER/chatter.csv")
+        tropes_file = os.path.join(self.args.datadir, "CHATTER/tropes.csv")
+        character_movie_map_file = os.path.join(self.args.datadir, "CHATTER/character-movie-map.csv")
+        dataset_df = pd.read_csv(dataset_file, index_col=None, dtype={"content-text": str})
         tropes_df = pd.read_csv(tropes_file, index_col="trope")
-        tensors_dir = os.path.join(self.data_dir, "60-modeling/tensors", self.data_type, self.tokenizer_model_name)
-        train_df = dataset_df[dataset_df["partition"] == "train"]
-        valid_df = dataset_df[dataset_df["partition"] == "dev"]
-        test_df = dataset_df[dataset_df["partition"] == "test"]
+        character_movie_map_df = pd.read_csv(character_movie_map_file, index_col=None, dtype=str)
+        tensors_dir = os.path.join(self.args.datadir, "60-modeling/tensors", self.args.inputtype,
+                                   self.args.tokenizermodel)
 
         # initialize model
-        if self.label_dependent:
-            model = LabelDependent(self.pretrained_model_name)
+        print("initializing model")
+        if self.args.labeldependent:
+            model = LabelDependent(self.args.pretrainedmodel)
             classifier = PortayClassifier(model.hidden_size)
         else:
-            model = LabelIndependent(self.pretrained_model_name)
+            model = LabelIndependent(self.args.pretrainedmodel)
             classifier = PortayClassifier(2 * model.hidden_size)
+        print("\n")
 
         # move model to gpu
+        print("moving model to gpu")
         model.cuda()
         classifier.cuda()
 
         # freeze encoder if flag is set
-        if self.freeze_encoder:
+        if self.args.freezeencoder:
             for parameter in model.encoder.parameters():
                 parameter.requires_grad = False
 
         # initialize optimizer
+        print("initializing optimizer")
         parameters = [{"params": model.non_encoder_parameters}, {"params": classifier.parameters()}]
-        if not self.freeze_encoder:
-            parameters.append({"params": model.encoder_parameters, "lr": self.encoderlr})
-        optimizer = AdamW(parameters, lr=self.lr)
+        if not self.args.freezeencoder:
+            parameters.append({"params": model.encoder_parameters, "lr": self.args.encoderlr})
+        optimizer = AdamW(parameters, lr=self.args.lr)
+        print("\n")
 
         # encode traits
-        tokenizer = AutoTokenizer.from_pretrained(self.pretrained_model_name, use_fast=True, add_prefix_space=True)
+        print("encoding trope definitions")
+        tokenizer = AutoTokenizer.from_pretrained(self.args.pretrainedmodel, use_fast=True, add_prefix_space=True)
         tropes = sorted(dataset_df["trope"].unique())
         definitions = [tropes_df.loc[trope, "definition"] for trope in tropes]
         trope_token_ids = (tokenizer(definitions, padding="max_length", truncation=True, return_tensors="pt")
                            .input_ids).cuda()
 
+        print("\n")
+
         # train model
-        if self.data_type == "story":
-            train_components = np.random.choice(train_df["component"].unique(), 10)
-            train_df = train_df[train_df["component"].isin(train_components)]
-            valid_components = np.random.choice(valid_df["component"].unique(), 5)
-            valid_df = valid_df[valid_df["component"].isin(valid_components)]
-            if self.label_dependent:
-                story_label_dependent_trainer.train(model, classifier, optimizer, train_df, valid_df, tropes,
-                                                    trope_token_ids, tensors_dir, self.n_epochs)
-            else:
-                story_label_independent_trainer.train(model, classifier, optimizer, train_df, valid_df, tropes,
-                                                      trope_token_ids, tensors_dir, self.n_epochs)
-        else:
-            train_characters = np.random.choice(train_df["character"].unique(), 53)
-            train_df = train_df[train_df["character"].isin(train_characters)]
-            valid_characters = np.random.choice(valid_df["character"].unique(), 10)
-            valid_df = valid_df[valid_df["character"].isin(valid_characters)]
-            if self.label_dependent:
-                character_label_dependent_trainer.train(model, classifier, optimizer, train_df, tropes, trope_token_ids,
-                                                        tensors_dir, self.n_epochs, self.ncharacters_batch)
-            else:
-                character_label_independent_trainer.train(model, classifier, optimizer, train_df, tropes,
-                                                          trope_token_ids, tensors_dir, self.n_epochs,
-                                                          self.ncharacters_batch)
+        if self.args.inputtype == "story":
+            print("input-type = story")
+            if self.args.labeldependent:
+                print("model-type = label-dependent\n\n")
+                story_label_dependent_trainer.train(model, classifier, optimizer, dataset_df, character_movie_map_df,
+                                                    tropes, trope_token_ids, tensors_dir, self.args.nepochs,
+                                                    self.args.ntropesbatch)
