b6fa714d771faa14b2b49290c38896befab167d4
sabyasachee <sbaruah@usc.edu>
2024-11-03 01:58:49 -0800

    use the datadirs and pretrained helper modules

--- 60-modeling/61-save-story-tensors.py
+++ 60-modeling/61-save-story-tensors.py
@@ -1,34 +1,32 @@
 """Create and save story tensors"""
 from dataloaders import tensorize
+from models import pretrained
+import datadirs
 
 import os
 import pandas as pd
 import torch
 import tqdm
-from transformers import AutoTokenizer
 
 from absl import app
 from absl import flags
 from absl import logging
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string("data_dir", default=None, help="data directory", required=True)
-flags.DEFINE_string("tokenizer", default=None, help="huggingface tokenizer name", required=True)
-flags.DEFINE_string("data_file", default="60-modeling/dataset-with-only-character-tropes.csv",
-                    help="data file")
-flags.DEFINE_string("scripts_dir", default="movie-scripts", help="movie scripts directory")
-flags.DEFINE_string("tensors_dir", default="60-modeling/tensors/story", 
-                    help="directory to save story tensors directory")
+flags.DEFINE_enum("model", default=None, enum_values=["roberta", "longformer"], help="model")
 
 def save_story_tensors(_):
-    data_dir = FLAGS.data_dir
-    tokenizer_model = FLAGS.tokenizer
-    data_file = os.path.join(data_dir, FLAGS.data_file)
-    scripts_dir = os.path.join(data_dir, FLAGS.scripts_dir)
-    tensors_dir = os.path.join(data_dir, FLAGS.tensors_dir, tokenizer_model)
-    data_df = pd.read_csv(data_file, index_col=None, dtype={"imdb-id": str, "content-text": str})
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_model, use_fast=True, add_prefix_space=True)
-    log_dir = os.path.join(tensors_dir, "logs")
+    model = FLAGS.model
+    datadir = datadirs.datadir
+    mapfile = os.path.join(datadir, "CHATTER/character-movie-map.csv")
+    scriptsdir = os.path.join(datadir, "movie-scripts")
+    tensorsdir = os.path.join(datadir, "60-modeling/tensors/story", model)
+    map_df = pd.read_csv(mapfile, index_col=None, dtype=str)
+    if model == "roberta":
+        tokenizer = pretrained.Tokenizer.roberta()
+    elif model == "longformer":
+        tokenizer = pretrained.Tokenizer.longformer()
+    log_dir = os.path.join(tensorsdir, "logs")
     os.makedirs(log_dir, exist_ok=True)
     logging.get_absl_handler().use_absl_log_file(program_name="save-story-tensors", log_dir=log_dir)
     logging.get_absl_handler().setFormatter(None)
@@ -39,12 +37,12 @@ def save_story_tensors(_):
     mentions_dfs = []
     utterances_dfs = []
 
-    for imdb_id, imdb_df in tqdm.tqdm(data_df.groupby("imdb-id"), total=data_df["imdb-id"].unique().size,
+    for imdb_id, imdb_df in tqdm.tqdm(map_df.groupby("imdb-id"), total=map_df["imdb-id"].unique().size,
                                       desc="reading segments and spans"):
-        imdb_characters = imdb_df["imdb-character"].unique()
-        segments_file = os.path.join(scripts_dir, imdb_id, "segments.csv")
-        mentions_file = os.path.join(scripts_dir, imdb_id, "mentions.csv")
-        utterances_file = os.path.join(scripts_dir, imdb_id, "utterances.csv")
+        imdb_characters = imdb_df["name"].unique()
+        segments_file = os.path.join(scriptsdir, imdb_id, "segments.csv")
+        mentions_file = os.path.join(scriptsdir, imdb_id, "mentions.csv")
+        utterances_file = os.path.join(scriptsdir, imdb_id, "utterances.csv")
         segments_df = pd.read_csv(segments_file, index_col=None)
         mentions_df = pd.read_csv(mentions_file, index_col=None)
         utterances_df = pd.read_csv(utterances_file, index_col=None)
@@ -56,14 +54,13 @@ def save_story_tensors(_):
 
     for i, (imdb_id, imdb_characters, segments_df, mentions_df, utterances_df) in enumerate(zip(
             imdb_ids, imdb_characters_arr, segments_dfs, mentions_dfs, utterances_dfs)):
-        logging.info(f"{i + 1:3d}/{len(imdb_ids)}. imdb-id = {imdb_id}")
+        logging.info(f"{i + 1:3d}/{len(imdb_ids)}\nimdb-id = {imdb_id}")
         try:
-            (story_token_ids, mentions_mask, utterances_mask, names_mask, mention_character_ids, 
-                utterance_character_ids) = (
+            (token_ids, mentions_mask, utterances_mask, names_mask, mention_character_ids, utterance_character_ids) = (
                 tensorize.create_tensors(tokenizer, imdb_characters, segments_df, mentions_df, utterances_df))
-            imdb_tensors_dir = os.path.join(tensors_dir, imdb_id)
+            imdb_tensors_dir = os.path.join(tensorsdir, imdb_id)
             os.makedirs(imdb_tensors_dir, exist_ok=True)
-            torch.save(story_token_ids, os.path.join(imdb_tensors_dir, "token-ids.pt"))
+            torch.save(token_ids, os.path.join(imdb_tensors_dir, "token-ids.pt"))
             torch.save(mentions_mask, os.path.join(imdb_tensors_dir, "mentions-mask.pt"))
             torch.save(utterances_mask, os.path.join(imdb_tensors_dir, "utterances-mask.pt"))
             torch.save(names_mask, os.path.join(imdb_tensors_dir, "names-mask.pt"))
@@ -71,7 +68,12 @@ def save_story_tensors(_):
             torch.save(utterance_character_ids, os.path.join(imdb_tensors_dir, "utterance-character-ids.pt"))
             with open(os.path.join(imdb_tensors_dir, "characters.txt"), "w") as fw:
                 fw.write("\n".join(imdb_characters))
-            logging.info("done")
+            logging.info(f"token-ids = {tuple(token_ids.shape)}")
+            logging.info(f"names-mask = {tuple(names_mask.shape)}")
+            logging.info(f"mentions-mask = {tuple(mentions_mask.shape)}")
+            logging.info(f"utterances-mask = {tuple(utterances_mask.shape)}")
+            logging.info(f"mention-character-ids = {tuple(mention_character_ids.shape)}")
+            logging.info(f"utterance-character-ids = {tuple(utterance_character_ids.shape)}")
         except Exception as e:
             logging.warning(str(e))
         logging.info("")
