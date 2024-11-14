aeeb14b5e180feb705fe2821686f00c1c501bbc6
sabyasachee <sbaruah@usc.edu>
2024-11-14 01:09:11 -0800

    rename evaluation module

--- 60-modeling/eval/evaluation.py
+++ 60-modeling/eval/evaluation.py
@@ -4,8 +4,8 @@ def evaluate(logits, labels):
     tn = ((logits < 0) & (labels == 0)).sum().item()
     fp = ((logits > 0) & (labels == 0)).sum().item()
     fn = ((logits < 0) & (labels == 1)).sum().item()
-    accuracy = (tp + tn)/(tp + tn + fp + fn)
-    precision = tp/(tp + fp)
-    recall = tp/(tp + fn)
+    accuracy = (tp + tn)/(tp + tn + fp + fn + 1e-23)
+    precision = tp/(tp + fp + 1e-23)
+    recall = tp/(tp + fn + 1e-23)
     f1 = 2 * precision * recall / (precision + recall)
     return 100*accuracy, 100*precision, 100*recall, 100*f1
