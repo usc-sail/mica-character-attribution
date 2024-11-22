df46ca530ff6f18ba34d2de893240bd30e858de8
sabyasachee <sbaruah@usc.edu>
2024-11-22 06:59:01 +0000

    add redondo

--- 60-modeling/datadirs.py
+++ 60-modeling/datadirs.py
@@ -3,5 +3,7 @@ import socket
 host = socket.gethostname()
 if host == "hermosa":
     datadir = "/data1/sbaruah/mica-character-attribution"
+elif host == "redondo":
+    datadir = "/data1/sbaruah/mica-character-attribution"
 elif host.endswith("hpc.usc.edu"):
     datadir = "/scratch1/sbaruah/mica-character-attribution"
