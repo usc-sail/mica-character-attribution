7d75b0cba90228abb60fab92658445209f68ad93
sabyasachee <sbaruah@usc.edu>
2024-11-22 06:58:17 +0000

    add sqlite, htop and gpustat

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-conda create -n attr python=3.11
+conda create -n chatter python=3.12
 
-conda install -c conda-forge numpy pandas tqdm selenium absl-py requests beautifulsoup4 unidecode scikit-learn scipy ipykernel matplotlib spacy cupy thefuzz nameparser jupyterlab ipython cinemagoer openai tiktoken tenacity boto3
+conda install -c conda-forge numpy pandas tqdm selenium absl-py requests beautifulsoup4 unidecode scikit-learn scipy ipykernel matplotlib spacy cupy thefuzz nameparser jupyterlab ipython cinemagoer openai tiktoken tenacity boto3 sqlite gpustat htop
 
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia
 conda install conda-forge::transformers
