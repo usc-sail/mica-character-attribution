f0d6cb131a0404a181eb5542bbbf1f0b96c7397e
sabyasachee <sbaruah@usc.edu>
2024-12-07 08:13:31 +0000

    add google apis, accelerate and bitsandbytes

--- env.sh
+++ env.sh
@@ -2,7 +2,10 @@
 
 conda create -n chatter python=3.12
 
-conda install -c conda-forge numpy pandas tqdm selenium absl-py requests beautifulsoup4 unidecode scikit-learn scipy ipykernel matplotlib spacy cupy thefuzz nameparser jupyterlab ipython cinemagoer openai tiktoken tenacity boto3 sqlite gpustat htop
+conda install -c conda-forge numpy pandas tqdm selenium absl-py requests beautifulsoup4 unidecode scikit-learn scipy ipykernel matplotlib spacy cupy thefuzz nameparser jupyterlab ipython cinemagoer openai tiktoken tenacity boto3 sqlite gpustat htop google-generativeai google-cloud-aiplatform ipywidgets
+pip install krippendorff pylcs
 
 conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia
 conda install conda-forge::transformers
+pip install accelerate
+pip install bitsandbytes
