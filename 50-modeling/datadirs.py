28347ad1ce48dad554b053228bf2c55f848a21b0
sabyasachee <sbaruah@usc.edu>
2025-02-04 10:47:28 +0000

    add data directory for AWS

--- 50-modeling/datadirs.py
+++ 50-modeling/datadirs.py
@@ -3,6 +3,11 @@ import socket
 
 host = socket.gethostname()
 if host == "redondo":
+    # lab server
     datadir = "/data1/sbaruah/mica-character-attribution"
 elif host.endswith("hpc.usc.edu"):
+    # university HPC compute
     datadir = "/scratch1/sbaruah/mica-character-attribution"
+else:
+    # AWS EC2
+    datadir = "/home/ubuntu/data/mica-character-attribution"
