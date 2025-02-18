d787e96633fc5468570517c9bd4172c78b81a0b8
sabyasachee <sbaruah@usc.edu>
2025-02-18 09:02:25 +0000

    add GPT prompter, simplify padding args, count tokens instead of estimating cost, bug fix in output decoding

--- 50-modeling/generation.py
+++ 50-modeling/generation.py
@@ -2,27 +2,33 @@
 import datadirs
 
 from absl import flags
-from accelerate import Accelerator, PartialState
+from accelerate import PartialState
 from accelerate.utils import gather_object
 from google.api_core import retry
 from google import generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 import math
+import openai
 import os
 import re
 import sys
+import tenacity
+import tiktoken
 import torch
 import tqdm
 from transformers import BitsAndBytesConfig, AutoTokenizer, AutoModelForCausalLM
 from vertexai.preview import tokenization
+import yaml
 
 # GEMINI
 flags.DEFINE_string("gemini_model", default=None, help="Gemini model")
 flags.DEFINE_string("gemini_key", default=None, help="Gemini key file")
-flags.DEFINE_bool("gemini_estimate_cost", default=False, help="Gemini estimate cost")
+flags.DEFINE_bool("gemini_count_tokens", default=False, help="Gemini count tokens")
 
 # GPT
 flags.DEFINE_string("gpt_model", default=None, help="GPT model")
+flags.DEFINE_string("gpt_key", default=None, help="OpenAI key file")
+flags.DEFINE_bool("gpt_count_tokens", default=False, help="GPT count tokens")
 
 # HUGGINGFACE
 flags.DEFINE_string("hf_model",
@@ -41,9 +47,8 @@ flags.DEFINE_bool("replace_system_role",
                   default=False,
                   help="substitute system role with user role if system role is not supported by tokenizer")
 flags.DEFINE_integer("batch_size", default=1, help="batch size")
-flags.DEFINE_integer("max_input_tokens", default=128, help="maximum tokens per input sequence in K (=2^10)")
+flags.DEFINE_integer("max_input_tokens", default=None, help="maximum tokens per input sequence in K (=2^10)")
 flags.DEFINE_integer("max_output_tokens", default=1, help="maximum tokens to generate")
-flags.DEFINE_enum("padding", default="longest", enum_values=["longest", "max_length"], help="padding strategy")
 flags.DEFINE_bool("do_sample", default=False, help="use sampling; otherwise use greedy decoding")
 flags.DEFINE_integer("top_k",
                      default=None,
@@ -55,19 +60,30 @@ flags.DEFINE_float("top_p",
 flags.DEFINE_float("temperature", default=1, help="temperature for generations")
 
 def models_checker(args):
+    gpt = args["gpt_model"] is not None
     gemini = args["gemini_model"] is not None
     hf = args["hf_model"] is not None
-    return not (gemini and hf)
+    return not (gpt and gemini and hf)
 
-def key_checker(args):
+def gpt_key_checker(args):
+    gpt = args["gpt_model"] is not None
+    key = args["gpt_key"] is not None
+    return not gpt or key
+
+def gemini_key_checker(args):
     gemini = args["gemini_model"] is not None
     key = args["gemini_key"] is not None
     return not gemini or key
 
-flags.register_multi_flags_validator(["gpt_model", "gemini_model", "hf_model"], models_checker,
+flags.register_multi_flags_validator(["gpt_model", "gemini_model", "hf_model"],
+                                     models_checker,
                                      message="Provide exactly one of gemini or hf models")
-flags.register_multi_flags_validator(["gemini_model", "gemini_key"], key_checker,
+flags.register_multi_flags_validator(["gemini_model", "gemini_key"],
+                                     gemini_key_checker,
                                      message="Provide API key file if you are using gemini model")
+flags.register_multi_flags_validator(["gpt_model", "gpt_key"],
+                                     gpt_key_checker,
+                                     message="Provide API key file if you are using gpt model")
 
 FLAGS = flags.FLAGS
 
@@ -90,6 +106,45 @@ def modelname():
         return f"{model_name_or_path}{bf16}{quant}"
     raise ValueError("model not provided")
 
+class GPT:
+    """GPT generator"""
+    def __init__(self):
+        print("configuring OpenAI API")
+        gpt_key_file = FLAGS.gpt_key
+        with open(gpt_key_file) as fr:
+            gpt_key_dict = yaml.load(fr, Loader=yaml.FullLoader)
+        self.client = openai.Client(api_key=gpt_key_dict["key"],
+                                    organization=gpt_key_dict["organization"],
+                                    project=gpt_key_dict["project"])
+        self.encoding = tiktoken.encoding_for_model(FLAGS.gpt_model)
+
+    @tenacity.retry(wait=tenacity.wait_random_exponential(min=1, max=60), stop=tenacity.stop_after_attempt(10))
+    def completion_with_backoff(self, **kwargs):
+        return self.client.chat.completions.create(**kwargs)
+
+    def __call__(self, prompts, system_instr=None):
+        if FLAGS.gpt_count_tokens:
+            ntokens_arr = [len(self.encoding.encode(prompt)) for prompt in tqdm.tqdm(prompts, desc="counting tokens")]
+            print(f"{sum(ntokens_arr)} input tokens, {FLAGS.max_output_tokens * len(prompts)} output tokens")
+            user_input = input("Do you want to proceed with prompting? [y/n] ")
+            if user_input.lower().strip() != "y":
+                sys.exit(0)
+        responses = []
+        for prompt in tqdm.tqdm(prompts, desc="prompting"):
+            messages = [{"role": "developer", "content": system_instr}] if system_instr is not None else []
+            messages.append({"role": "user", "content": prompt})
+            output = self.completion_with_backoff(messages=messages,
+                                                  model=FLAGS.gpt_model,
+                                                  max_completion_tokens=FLAGS.max_output_tokens,
+                                                  temperature=FLAGS.temperature,
+                                                  top_p=FLAGS.top_p)
+            try:
+                response = output.choices[0].message.content
+            except Exception:
+                response = "ERROR"
+            responses.append(response)
+        return responses
+
 class Gemini:
     """Gemini generator"""
     def __init__(self, system_instr=None):
@@ -101,19 +156,12 @@ class Gemini:
         self.model = genai.GenerativeModel(model_name=FLAGS.gemini_model,
                                            system_instruction=system_instr)
         self.tokenizer = tokenization.get_tokenizer_for_model(FLAGS.gemini_model)
-        self.pro = "pro" in FLAGS.gemini_model
 
     def __call__(self, prompts):
-        if FLAGS.gemini_estimate_cost:
-            if self.pro:
-                input_token_rate, output_token_rate = 1.25, 5
-            else:
-                input_token_rate, output_token_rate = 0.075, 0.3
+        if FLAGS.gemini_count_tokens:
             ntokens_arr = [self.tokenizer.count_tokens(prompt).total_tokens
                            for prompt in tqdm.tqdm(prompts, desc="counting tokens")]
-            cost = (sum(ntokens_arr) * input_token_rate
-                    + len(prompts) * FLAGS.max_output_tokens * output_token_rate)/(1<<17)
-            print(f"estimated cost = ${cost:.2f}")
+            print(f"{sum(ntokens_arr)} input tokens, {FLAGS.max_output_tokens * len(prompts)} output tokens")
             user_input = input("Do you want to proceed with prompting? [y/n] ")
             if user_input.lower().strip() != "y":
                 sys.exit(0)
@@ -123,8 +171,11 @@ class Gemini:
                          HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
                          HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE}
         generation_config = genai.GenerationConfig(temperature=FLAGS.temperature,
-                                                   max_output_tokens=FLAGS.max_output_tokens)
+                                                   max_output_tokens=FLAGS.max_output_tokens,
+                                                   top_k=FLAGS.top_k,
+                                                   top_p=FLAGS.top_p)
         request_opts = genai.types.RequestOptions(retry=retry.Retry(initial=10, multiplier=2, maximum=60, timeout=300))
+        responses = []
         for prompt in tqdm.tqdm(prompts, desc="prompting"):
             output = self.model.generate_content(prompt,
                                                  safety_settings=safety_settings,
@@ -134,7 +185,8 @@ class Gemini:
                 response = output.candidates[0].content.parts[0].text
             except Exception:
                 response = "ERROR"
-            yield response
+            responses.append(response)
+        return responses
 
 class HF:
     """Huggingface model generator"""
@@ -159,17 +211,18 @@ class HF:
                                                           torch_dtype=compute_dtype,
                                                           quantization_config=quantization_config,
                                                           device_map={"": self.partial_state.process_index},
-                                                          attn_implementation=FLAGS.attn)
+                                                          attn_implementation=FLAGS.attn,
+                                                          trust_remote_code=True)
         self.partial_state.print(f"generation config: {self.model.generation_config}")
         self.partial_state.print("instantiating tokenizer")
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path,
                                                        padding_side="left",
-                                                       truncation_side="right")
+                                                       truncation_side="right",
+                                                       trust_remote_code=True)
         if self.tokenizer.pad_token_id is None:
             self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
 
     def __call__(self, prompts, system_instr=None):
-        max_input_tokens = (1 << 10) * FLAGS.max_input_tokens
         if system_instr is not None:
             if FLAGS.replace_system_role:
                 messages = [[{"role": "user", "content": f"{system_instr}\n{prompt}"}] for prompt in prompts]
@@ -189,10 +242,11 @@ class HF:
                 batch_prompts = process_prompts[i * FLAGS.batch_size: (i + 1) * FLAGS.batch_size]
                 batch_encoding = (self
                                   .tokenizer(batch_prompts,
-                                             padding=FLAGS.padding,
-                                             truncation=True,
+                                             padding="max_length" if FLAGS.max_input_tokens is not None else "longest",
+                                             truncation=FLAGS.max_input_tokens is not None,
                                              return_tensors="pt",
-                                             max_length=max_input_tokens)
+                                             max_length=((1 << 10) * FLAGS.max_input_tokens 
+                                                         if FLAGS.max_input_tokens is not None else None))
                                   .to(self.partial_state.device))
                 batch_output = self.model.generate(**batch_encoding,
                                                    do_sample=FLAGS.do_sample,
@@ -202,7 +256,7 @@ class HF:
                                                    max_new_tokens=FLAGS.max_output_tokens,
                                                    pad_token_id=self.tokenizer.pad_token_id,
                                                    return_legacy_cache=True)
-                batch_output = batch_output[:,-FLAGS.max_output_tokens:]
+                batch_output = batch_output[:, batch_encoding["input_ids"].shape[1]:]
                 batch_responses = self.tokenizer.batch_decode(batch_output, skip_special_tokens=True)
                 process_responses.extend(batch_responses)
             responses = [process_responses]
